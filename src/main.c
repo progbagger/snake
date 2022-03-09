f63a96e4088900b55a6747d4035a3ab5b09e9e36
Danil <thrixstudio@yandex.ru>
2022-03-10 00:21:40 +0700

    define instead of constants

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-const int n = 80, m = 80;
+#define N 80
+#define M 80
 
 int **mem_alloc();
 void mem_free(int **arr);
@@ -15,6 +16,7 @@ void snake_start_pos(int **field, int **head, int **tail, int size);
 
 int main() {
     work();
+    return 0;
 }
 
 // основная работяга
@@ -38,24 +40,24 @@ void snake_start_pos(int **field, int **head, int **tail, int size) {
         координата головы по n будет n / 2,
         а по m примерно m / 2 - 5
     */
-    *head = field[n / 2] + m / 2 - 5;
+    *head = field[N / 2] + M / 2 - 5;
     **head = 1;
     for (int i = 1; i < size; i++)
-        *(head[n / 2] - size) = 1;
-    *tail = field[n / 2] + m / 2 - 5 - size + 1;
+        *(head[N / 2] - size) = 1;
+    *tail = field[N / 2] + M / 2 - 5 - size + 1;
 }
 
 // выделение памяти под поле
 int **mem_alloc() {
-    int **result = malloc(n * sizeof(int*));
-    for (int i = 0; i < n; i++)
-        result[i] = malloc(m * sizeof(int));
+    int **result = malloc(N * sizeof(int*));
+    for (int i = 0; i < N; i++)
+        result[i] = malloc(M * sizeof(int));
     return result;
 }
 
 // очистка памяти
 void mem_free(int **arr) {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < N; i++)
         free(arr[i]);
     free(arr);
 }
