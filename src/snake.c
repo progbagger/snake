e2deceacc27f0edf9406f8a0e93d502c2be903bb
Danil <thrixstudio@yandex.ru>
2022-03-12 02:29:00 +0700

    struct snake

--- src/snake.c
+++ src/snake.c
@@ -1,47 +1,56 @@
 #include "snake.h"
 #include <stdlib.h>
 
-struct node *init(int ii, int jj) {
-    struct node *head = NULL;
-    head = malloc(sizeof(snake));
-    head->i = ii;
-    head->j = jj;
-    head->next = NULL;
-    head->size = 1;
-    return NULL;
+snake *init(int ii, int jj) {
+    snake *p = NULL;
+    p = malloc(sizeof(snake));
+    p->head->i = ii;
+    p->head->j = jj;
+    p->head->next = NULL;
+    p->size = 1;
+    return p;
 }
 
-struct node *push_back(struct node *head, int ii, int jj) {
-    struct node *p = head;
-    struct node *result = init(ii, jj);
+snake *push_back(snake *head, int ii, int jj) {
+    struct node *result = malloc(sizeof(struct node));
+    head->size++;
+    result->i = ii;
+    result->j = jj;
+    result->next = NULL;
+    struct node *p = head->head;
     while (p->next != NULL)
         p = p->next;
     p->next = result;
-    return result;
-}
-
-struct node *push_forward(struct node *head, int ii, int jj) {
-    struct node *p = init(ii, jj);
-    p->next = head;
-    head = p;
     return head;
 }
 
-void destroy(struct node *head) {
-    while (head != NULL) {
-        struct node *p = head;
-        head = head->next;
-        free(p);
+snake *push_forward(snake *head, int ii, int jj) {
+    struct node *p = malloc(sizeof(struct node));
+    head->size++;
+    p->i = ii;
+    p->j = jj;
+    p->next = head->head->next;
+    head->head = p;
+    return head;
+}
+
+void destroy(snake *head) {
+    struct node *p = head->head;
+    while (p != NULL) {
+        struct node *pp = p;
+        p = p->next;
+        free(pp);
     }
+    free(head);
 }
 
-void move_snake(struct node *head, int ii, int jj) {
+void move_snake(snake *head, int ii, int jj) {
     delete_tail(head);
-    head = push_forward(head, ii, jj);
+    push_forward(head, ii, jj);
 }
 
-void delete_tail(struct node *head) {
-    struct node *p = head;
+void delete_tail(snake *head) {
+    struct node *p = head->head;
     while (p->next != NULL) {
         p = p->next;
     }
