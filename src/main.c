c198169ed18eb3436ef868928fc8f8690c5b22be
progbagger <thrixstudio@yandex.ru>
2022-04-16 01:55:14 +0700

    develop - Deleted previous TODO

--- src/main.c
+++ src/main.c
@@ -1,12 +1,5 @@
 #include "snake.h"
 
-/*
-    TODO: Redesign game interface so it will
-    TODO: consume whole screen of terminal
-
-    TODO: Buffered input!
-*/
-
 int main() {
     game();
     return 0;
