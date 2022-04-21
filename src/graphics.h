e755df94459182983ff294b5a27c491aa8c0b7c0
progbagger <thrixstudio@yandex.ru>
2022-04-21 23:57:05 +0700

    reorganization: Fixed bug with unprintable (0, 0) field cell

---
+++ src/graphics.h
@@ -0,0 +1,12 @@
+#ifndef SRC_GRAPHICS_H_
+#define SRC_GRAPHICS_H_
+
+#include "snake.h"
+
+void print_field(Snake *s);
+void change_field(Snake *s);
+
+void menu_level();
+void menu_speed();
+
+#endif  // SRC_GRAPHICS_H_
